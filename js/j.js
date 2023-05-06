845377d56d21aef39677212ffd2a4888480537cf
SalmaEssam7 <122183927+SalmaEssam7@users.noreply.github.com>
2023-05-07 02:59:46 +0300

    Add files via upload

---
+++ js/j.js
@@ -0,0 +1,78 @@
+
+
+$(document).ready(function(){
+  
+    $('.btn1').click(function(){
+        $(".a").slideToggle();
+        $(".a").animate({'width':200});
+        $(".a").animate({'left':100});
+        $(".a").animate({'height':300});
+        $(".a").animate({'opacity':0.2}); 
+    
+    
+        
+    });
+   
+
+});
+
+
+$(document).ready(function(){
+  
+    $('.btn2').click(function(){
+    
+        $(".b").slideToggle();
+        $(".b").animate({'width':200});
+        $(".b").animate({'right':500});
+        $(".b").animate({'height':300});
+        $(".b").animate({'opacity':0.2});
+            
+        
+    });
+   
+
+});
+
+
+
+
+$(document).ready(function(){
+  
+    $('.btn3').click(function(){
+    
+        $(".c").slideToggle();
+        $(".c").animate({'width':200});
+        $(".c").animate({'left':400});
+        $(".c").animate({'height':300});
+        $(".c").animate({'opacity':0.2});
+            
+        
+    });
+   
+
+});
+
+
+$(document).ready(function(){
+  
+    $('.btn4').click(function(){
+    
+        $(".d").slideToggle();
+        $(".d").animate({'width':200});
+        $(".d").animate({'left':400});
+        $(".d").animate({'height':300});
+        $(".d").animate({'opacity':0.2});
+            
+        
+    });
+   
+
+});
+
+
+
+
+
+
+
+
---
+++ js/java.js
@@ -0,0 +1,366 @@
+$(document).ready(function() {
+
+    $("#M").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(127, 167, 219) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(8, 93, 189)')
+    }
+});
+$(document).ready(function() {
+
+    $("#E").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(127, 167, 219) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(8, 93, 189)')
+    }
+});
+$(document).ready(function() {
+
+    $("#S").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(127, 167, 219) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(8, 93, 189)')
+    }
+});
+$(document).ready(function() {
+
+    $("#act1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(8, 93, 189) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $("#act2").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(8, 93, 189) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $("#act22").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(8, 93, 189) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $("#act222").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(8, 93, 189) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $("#act2222").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(8, 93, 189) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".city").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(7, 23, 77) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(127, 168, 243)')
+    }
+});
+$(document).ready(function() {
+$(".city1").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city2").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city3").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city4").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city5").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city6").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city7").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city8").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+$(".city9").hover(fun_in, fun_out);
+
+function fun_in() {
+    $(this).css('background', 'rgb(7, 23, 77) ')
+}
+
+function fun_out() {
+    $(this).css('background', 'rgb(127, 168, 243)')
+}
+});
+$(document).ready(function() {
+
+    $(".actA").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(8, 93, 189) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+
+$(document).ready(function() {
+
+    $(".u1H1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u1C1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u1R1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u1T1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+
+$(document).ready(function() {
+
+    $(".u2H1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128)')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u2C1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u2R1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u2T1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+
+$(document).ready(function() {
+
+    $(".u3H1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128)')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u3C1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128)')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u3R1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128)')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+$(document).ready(function() {
+
+    $(".u3T1").hover(fun_in, fun_out);
+
+    function fun_in() {
+        $(this).css('background', 'rgb(33, 66, 128) ')
+    }
+
+    function fun_out() {
+        $(this).css('background', 'rgb(7, 23, 77)')
+    }
+});
+
